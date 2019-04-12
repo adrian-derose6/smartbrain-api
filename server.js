840704458c4113e30d043ab63a91d10c29bf5082
Adrian DeRose <adrian.derose6@gmail.com>
2019-04-11 23:22:57 -0500

    Return user information upon registration

--- server.js
+++ server.js
@@ -73,16 +73,16 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    db.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    database.select('*').from('users').where({ id })
+    .then(users => {
+        if (users.length) {
+            res.json(users[0])
+        }
+        else {
+            res.status(400).json('Not found')
         }
     })
-    if (!found) {
-        res.status(400).json('not found');
-    }
+    .catch(err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
